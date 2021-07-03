346b7dc9788fc63d9fb7a456a17b0008d1f63752
Frank520lang <42695465+Frank520lang@users.noreply.github.com>
2021-07-03 15:51:05 +0800

    frank

---
+++ src/simple/MaxSubArray.java
@@ -0,0 +1,22 @@
+package simple;
+
+public class MaxSubArray {
+    //构造方法
+    //    public MaxSubArray() {
+    //        super();
+    //    }
+
+    public static int maxSubArray(int[] nums) {
+        int index=nums[0];
+        int sum=nums[0];
+        //
+        for (int i=0;i<nums.length;i++) {
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int nums[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        maxSubArray(nums);
+    }
+}
