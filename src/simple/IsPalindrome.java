c4853d17b33fe9d92465f49c232aeb17054d431a
Frank520lang <42695465+Frank520lang@users.noreply.github.com>
2022-01-20 11:14:29 +0800

    Update IsPalindrome.java
    回文数

--- src/simple/IsPalindrome.java
+++ src/simple/IsPalindrome.java
@@ -1,19 +1,40 @@
 package simple;
 
+/*
+给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+思路：   1，判断数组的正负
+         2，将数字的后半部分反转与前半部分比较（临界条件：x<=palindrome）
+*/
 public class IsPalindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(123321));
+        System.out.println("123321:" + isPalindrome(123321));
+        System.out.println("12321:" + isPalindrome(12321));
+        System.out.println("1232:" + isPalindrome(1232));
+        System.out.println("03445:" + isPalindrome(03445));
+        System.out.println("-123321:" + isPalindrome(-123321));
     }
 
     public static boolean isPalindrome(int x) {
-        //思考：这里大家可以思考一下，为什么末尾为 0 就可以直接返回 false
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        int revertedNumber = 0;
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
+        if (x < 0) {
+            return false;
+        }
+        int palindrome = 0;
+        /*
+         * x palindrome
+         * 1233321 0
+         * 12332 1
+         * 1233 12
+         * 123 123
+         */
+        // 边界条件
+        while (x > palindrome) {
+            // palindrome与x的关系
+            palindrome = palindrome * 10 + x % 10;
+            // 循环控制条件
             x /= 10;
         }
-        return x == revertedNumber || x == revertedNumber / 10;
+        // 判断回文数
+        return x == palindrome || x == palindrome / 10;
     }
 
 }
